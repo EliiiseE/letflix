caf797f0c04ae3a99c875ab22d0c6963d7be5e04
EliiiseE <elise.echasseriau@mmibordeaux.com>
2021-11-25 18:48:01 +0100

    movieInfo component v1

---
+++ src/components/MovieInfo/index.js
@@ -0,0 +1,35 @@
+
+import React from 'react'
+import styles from './MovieInfo.module.sass'
+import MovieList from 'components/MovieList'
+import { Icon } from '@iconify/react'
+
+const MovieInfo = (props) => {
+  
+    const movies = [{ image: 'images/bright.png', description: 'yo' },
+    { image: 'images/bright.png', description: 'yop' },
+    { image: 'images/bright.png', description: 'yo' },
+    { image: 'images/bright.png', description: 'yop' }]
+  return (
+   
+    <div className={styles.container}>
+        <Icon icon="akar-icons:chevron-left" />
+        <div className={styles.information_top}>
+            <img src={props.image} className={styles.image}/>
+            <div className={styles.information_top_text}>
+                <h2>{props.name}</h2>
+                <p>{props.year}</p>
+                <p>{props.age}</p>
+                <p>{props.duration}</p>
+            </div>
+        </div>
+        <a href="#">Play</a>
+        <div className={styles.description}>
+            <p>Em um mundo futurista, seres humanos convivem em harmônia com seres fantásticos, como fadas e ogros. Mesmo nesse cenário infrações da lei acontecem e um policial humano (Will Smith) especializado em crimes mágicos é obrigado a trabalhar junto com um orc (Joel Edgerton) para evitar que uma poderosa arma caia nas mãos erradas.</p> 
+        </div>
+        <MovieList movies={movies} title="Films en lien" />
+    </div> 
+  )
+}
+
+export default MovieInfo
--- src/pages/Movie/index.js
+++ src/pages/Movie/index.js
@@ -1,14 +1,13 @@
 
 import React from 'react'
 import styles from './Movie.module.sass'
-import Layout from 'components/Layout'
+import MovieInfo from 'components/MovieInfo'
 
-const Movie = () => {
+const Movie = (props) => {
   
   return (
-   
     <div className={styles.container}>
-      <Layout />
+      <MovieInfo image='images/bright.png' name='Titanic' year='1669' age='16+' duration='1h24'/>
     </div> 
   )
 }
