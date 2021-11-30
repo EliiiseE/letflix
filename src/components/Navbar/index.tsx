29d4c71f8d004f697b1c8dcc6269e3c0bc3c87ba
BlancAthur <blancarthur33170@gmail.com>
2021-11-30 21:36:37 +0100

    search-bar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -19,11 +19,7 @@ const Navbar = (): ReactElement => {
         </li>
 
         <li className={styles.link}>
-<<<<<<< HEAD
-          <Link to={`search`}>
-=======
           <Link to={`/Search`}>
->>>>>>> db6ec608fecac0a8de90a3aed34cf516a21445e1
             <TextIcon titleName="Rechercher" icon="akar-icons:search" />
           </Link>
         </li>
--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -2,6 +2,7 @@ import Layout from 'components/Layout';
 import { ReactElement, useEffect } from 'react';
 import { Helmet } from 'react-helmet-async';
 import styles from './Search.module.sass';
+import { Icon } from '@iconify/react';
 
 const Search = (): ReactElement => {
   // Always return to the top of the page
@@ -16,9 +17,13 @@ const Search = (): ReactElement => {
         <meta name="description" content="Description" />
       </Helmet>
       <div className={styles.container}>
-        <h1>salut</h1>
-        <input className={styles.inputName} type="text" placeholder="film"></input>
-        {/* <button>Ok</button> */}
+        <input
+          className={styles.inputName}
+          type="text"
+          placeholder="Quel Film ?"
+          src="public/flavicon1.ico"
+        ></input>
+        <Icon className={styles.search} icon="akar-icons:search" />
       </div>
     </Layout>
   );
