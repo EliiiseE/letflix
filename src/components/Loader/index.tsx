23c2f48054bde526ffba2a8071db9da63297a895
BlancAthur <blancarthur33170@gmail.com>
2021-12-03 13:11:49 +0100

    loader

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,22 +1,23 @@
 import { useEffect, useState } from 'react';
 
 import Styles from './Loader.module.sass';
+import classnames from 'classnames';
 
 function Loader() {
   const [isLoading, setIsLoading] = useState(true);
 
-  console.log(Styles);
-
-    useEffect(() => {
-      const timer = setTimeout(() => {
-        setIsLoading(false);
-      }, 3000);
-      return () => clearTimeout(timer);
-    }, []);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <>
-      <h1 className={isLoading ? Styles.tracking_in_contract : Styles.transition_end}>Letflix</h1>
+      <h1 className={classnames(Styles.tracking_in_contract, !isLoading && Styles.transition_end)}>
+        Letflix
+      </h1>
     </>
   );
 }
